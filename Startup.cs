67c144e86d84fa7768138de0001f78a44fd72a03
Risner <ar306514@ohio.edu>
2020-02-04 22:09:03 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(risner_MIS4200.Startup))]
+namespace risner_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
