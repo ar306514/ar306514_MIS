e3726e60aad42d0e1b802194af29a33a44ce624a
Risner, Alexis <ar306514@ohio.edu>
2020-02-10 00:33:12 -0500

    Added New class model and updated CustomerCS

---
+++ Models/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace risner_MIS4200.Models
+{
+    public class Professor
+    {
+        public int professorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        // add any other fields as appropriate
+        //Product is on the "one" side of a one-to-many relationship with OrderDetail
+        //we indicate that with an ICollection
+        public ICollection<CourseDetail> CourseDetail { get; set; }
+    }
+}
+
+ public class orderDetail
+{
+    public int coursedetailID { get; set; }
+    public int  { get; set; }
+    public decimal price { get; set; }
+    // the next two properties link the orderDetail to the Order
+    public int orderID { get; set; }
+    public virtual Order Order { get; set; }
+    // the next two properties link the orderDetail to the Product
+    public int productID { get; set; }
+    public virtual Product Product { get; set; }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,31 +1,58 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Class1
+namespace risner_MIS4200 //NOTE: luce_MIS4200 is the name of the sample solution
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	public int customerID { get; set; }
-	public string firstName { get; set; }
-	public string lastName { get; set; }
-	public string email { get; set; }
-	public string phone { get; set; }
-	public DateTime customerSince { get; set; }
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime graduationYear { get; set; }
 
-	// add any other fields as appropriate
-	// a customer can have any number of orders, a 1:M relationship,
-	// We represent this in the model with an ICollection
-	// The syntax says we are creating an ICollection of Order objects,
-	// (the name inside the <> is the object name),
-	// and the local name of the collection will be Order
-	// (the object name and the local name do not have to be the same)
-	public ICollection<Order> Order { get; set; }
-}
-}
+        // add any other fields as appropriate
+        // a customer can have any number of orders, a 1:M relationship,
+        // We represent this in the model with an ICollection
+        // The syntax says we are creating an ICollection of Order objects,
+        // (the name inside the <> is the object name),
+        // and the local name of the collection will be Order
+        // (the object name and the local name do not have to be the same)
+        public ICollection<Course> Course { get; set; }
+    }
 
+    // TODO: Add constructor logic here
+    //
+    // add any other fields as appropriate
+    // a customer can have any number of orders, a 1:M relationship,
+    // We represent this in the model with an ICollection
+    // The syntax says we are creating an ICollection of Order objects,
+    // (the name inside the <> is the object name),
+    // and the local name of the collection will be Order
+    // (the object name and the local name do not have to be the same)
+    
+        public class Course
+        {
+            [Key]
+            public int courseID { get; set; }
+            public string description { get; set; }
+            public DateTime startDate { get; set; }
+            public DateTime endDate { get; set; }
+        // add any other fields as appropriate
+        //Order is on the "one" side of a one-to-many relationship with OrderDetail
+        //and we indicate that with an ICollection
+        public ICollection<couresDetail> courseDetail { get; set; }
+            //Order is on the Many side of the one-to-many relation between Customer
+            //and Order and we represent that relationship like this
+            public int studentID { get; set; }
+            public virtual Student Student { get; set; }
+       
+        }
+
+    
+}
 
