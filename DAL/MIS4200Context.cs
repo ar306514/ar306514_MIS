9984e238cb7b2a7251b50afd3da7ccb3b4c1fd7d
Risner, Alexis <ar306514@ohio.edu>
2020-02-10 16:34:03 -0500

    updated again

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using risner_MIS4200.Models; // This is needed to access the models
+using ar306514_MIS.Models; // This is needed to access the models
 using System.Data.Entity; // this is needed to access the DbContext object
 
 
-namespace risner_MIS4200.context
+namespace ar306514_MIS.context
 {
     public class MIS4200Context : DbContext // inherits from DbContext
     {
@@ -20,7 +20,6 @@ namespace risner_MIS4200.context
         // and is the name used to reference the entity in code
         public DbSet<Course> Course { get; set; }
         public DbSet<Student> Student { get; set; }
-        public DbSet<Professor> Professor { get; set; }
-        public DbSet<CourseGrade> CourseGrade { get; set; }
+        
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations;
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-namespace risner_MIS4200 //NOTE: luce_MIS4200 is the name of the sample solution
+namespace ar306514_MIS.Models //NOTE: luce_MIS4200 is the name of the sample solution
 {
     public class Student
     {
@@ -37,25 +37,25 @@ namespace risner_MIS4200 //NOTE: luce_MIS4200 is the name of the sample solution
     // (the name inside the <> is the object name),
     // and the local name of the collection will be Order
     // (the object name and the local name do not have to be the same)
-    
-        public class Course
-        {
-            [Key]
-            public int courseID { get; set; }
-            public string description { get; set; }
-            public DateTime startDate { get; set; }
-            public DateTime endDate { get; set; }
+
+    public class Course
+    {
+        [Key]
+        public int courseID { get; set; }
+        public string description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
         // add any other fields as appropriate
         //Order is on the "one" side of a one-to-many relationship with OrderDetail
         //and we indicate that with an ICollection
-        public ICollection<CourseGrade> courseGrade { get; set; }
-            //Order is on the Many side of the one-to-many relation between Customer
-            //and Order and we represent that relationship like this
-            public int studentID { get; set; }
-            public virtual Student Student { get; set; }
-       
-        }
 
-    
+        //Order is on the Many side of the one-to-many relation between Customer
+        //and Order and we represent that relationship like this
+        public int studentID { get; set; }
+        public virtual Student Student { get; set; }
+
+    }
+
+
 }
 
