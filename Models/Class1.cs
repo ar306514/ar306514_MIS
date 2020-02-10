000bee6a36b2643b6d55939c839182e98a9c80f4
Risner, Alexis <ar306514@ohio.edu>
2020-02-10 16:03:06 -0500

    first half of first page done

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 
 /// <summary>
 /// Summary description for Class1
@@ -14,19 +18,18 @@ namespace risner_MIS4200.Models
         // add any other fields as appropriate
         //Product is on the "one" side of a one-to-many relationship with OrderDetail
         //we indicate that with an ICollection
-        public ICollection<CourseDetail> CourseDetail { get; set; }
+        public ICollection<CourseGrade> CourseGrade { get; set; }
     }
 }
 
- public class orderDetail
+ public class CourseGrade
 {
     public int coursedetailID { get; set; }
-    public int  { get; set; }
-    public decimal price { get; set; }
+    public decimal grade { get; set; }
     // the next two properties link the orderDetail to the Order
-    public int orderID { get; set; }
-    public virtual Order Order { get; set; }
+    public int courseID { get; set; }
+    public virtual Course Course { get; set; }
     // the next two properties link the orderDetail to the Product
-    public int productID { get; set; }
-    public virtual Product Product { get; set; }
+    public int professorID { get; set; }
+    public virtual Professor Professor { get; set; }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.ComponentModel.DataAnnotations;
 
 /// <summary>
@@ -45,7 +48,7 @@ namespace risner_MIS4200 //NOTE: luce_MIS4200 is the name of the sample solution
         // add any other fields as appropriate
         //Order is on the "one" side of a one-to-many relationship with OrderDetail
         //and we indicate that with an ICollection
-        public ICollection<couresDetail> courseDetail { get; set; }
+        public ICollection<CourseGrade> courseGrade { get; set; }
             //Order is on the Many side of the one-to-many relation between Customer
             //and Order and we represent that relationship like this
             public int studentID { get; set; }
